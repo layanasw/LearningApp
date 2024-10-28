37d81ca89e048e5dce2bc0a55b02d5aaffe7e9d9
layanasw <lyan.wasaidi2@gmail.com>
2024-10-28 15:11:12 +0300

    Initial Commit

---
+++ learningF/learningFApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningFApp.swift
+//  learningF
+//
+//  Created by layan alwasaidi on 28/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct learningFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
